d2f3e760ccaf051c0dec488254c333d238e6caed
Brendan Ross <brend@DESKTOP-BFUM62J>
2026-02-16 18:02:14 -0700

    Added more descriptive text for functions.

--- SimpleEncryptor.h
+++ SimpleEncryptor.h
@@ -15,16 +15,20 @@ class SimpleEncryptor
 public:
 
     /**
-    * Encrypts a message using the custom pseudorandom number generator. The encrypted message is written to a .txt file.
-    * @param message The message to be encrypted
-    * @return The encrypted message as a string
+    * Encrypts a message using the custom pseudorandom number generator. The encrypted message is written to a .txt file, titled "key.txt".
+    * This file contains the key used to encrypt the message, which is necessary for decryption. 
+    * 
+    * WARNING: This file will be overwritten each time the encryptMessage function is called.
+    * 
+    * @param message The message to be encrypted.
+    * @return The encrypted message as a string.
     */
     std::string encryptMessage(const std::string& message);
 
     /**
-    * Decrypts a message from a .txt file that was encrypted using the encryptMessage function. The decrypted message is returned as a string.
+    * Decrypts a message with the provided .txt file. The decrypted message is returned as a string.
     * @param messageToDecrypt The message to be decrypted.
-    * @param keyFilename The filename of the .txt. file containing the key used to encrypt the message.
+    * @param keyFilename The filename of the .txt file. Used as the key to decrypt the message.
     * @return The decrypted message as a string. Returns an empty string if the file cannot be opened.
     */
     std::string decryptMessage(const std::string& messageToDecrypt, const std::string& keyFilename);
